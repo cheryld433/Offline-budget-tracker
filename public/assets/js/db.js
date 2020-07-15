f32f4b2496319e46a2b24126d73fae0761173bf8
Cheryl Daniels <cheryld433@gmail.com>
2020-07-15 00:22:58 -0400

    updated db.js

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -48,10 +48,10 @@ function checkDatabase() {
     }
   };
 }
-function deletePending() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-  store.clear();
-}
+// function deletePending() {
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+//   store.clear();
+// }
 
 window.addEventListener("online", checkDatabase);
