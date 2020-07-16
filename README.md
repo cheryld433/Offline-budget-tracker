d8b666d9142197853ce7cfb50366beba7d0720bd
Cheryl Daniels <cheryld433@gmail.com>
2020-07-16 00:08:30 -0400

    Added readMe, modified js and json files

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Offline-budget-tracker
+# BUDGET TRACKER
+
+![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen)
+
+## Description: 
+With this application the user will be able to track  their withdrawals and deposits with or without a data/internet connection. When traveling the users account balance is accurate. When entering transactions offline, the app will populate the total when back online.
+
+## Table of Content:
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributors](#contributors)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+
+## Installation:
+npm install
+
+## Usage:
+
+
+
+## Contributors:
+Cheryl Daniels https://cheryld433.github.io/Portfolio/
+
+## Tests:
+
+
+#### Questions:
+
--- public/db.js
+++ public/db.js
@@ -1,18 +1,14 @@
 let db;
 
-//Creates a new DB request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
-    //Creates the "Pending" object store and sets the autoIncrement to "true"
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
     db = event.target.result;
-
-    //Checks if the app is online before reading from the DB:
     if (navigator.onLine) {
         checkDatabase();
     }
@@ -22,22 +18,15 @@ request.onerror = function(event) {
     console.log("Error found: " + event.target.errorCode);
 };
 
-//saveRecord needs to be defined, otherwise, error is thrown when trying to run:
 function saveRecord(record) {
-    //With readwrite access, the transaction is created on the pending DB
     const transaction = db.transaction(["pending"], "readwrite");
-    //Pending object store is accessed
     const store = transaction.objectStore("pending");
-    //Record is added to store with add method
     store.add(record);
 }
 
 function checkDatabase() {
-    //Transaction is opened on the pending DB
     const transaction = db.transaction(["pending"], "readwrite");
-    //Accesses pending object store
     const store = transaction.objectStore("pending");
-    //Obtains all records from store and sets to variables
     const getAll = store.getAll();
 
     getAll.onsuccess = function() {
@@ -52,16 +41,12 @@ function checkDatabase() {
             })
             .then(response => response.json())
             .then(() => {
-                //Opens transaction on pending DB if successful
                 const transaction = db.transaction(["pending"], "readwrite");
-                //Accesses pending object store
                 const store = transaction.objectStore("pending");
-                //Clears all items in store
                 store.clear();
             });
         }
     };
 }
 
-//Listens for when the app comes back online
 window.addEventListener("online", checkDatabase);
